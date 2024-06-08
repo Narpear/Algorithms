da111516cb4ab14adc4c6a49381e698bfbb41aa5
Narpear <prer.kulk@gmail.com>
2024-06-08 21:33:41 +0530

    created bubble_sort.cpp

--- Sorting Algorithms/bubble_sort.cpp
+++ Sorting Algorithms/bubble_sort.cpp
@@ -5,6 +5,23 @@ using namespace std;
 int main(){
     vector<int> arr = {7, 4, 8, 2, 9, 4, 9, 2, 0, 1, 8, 3, 7};
     int n = arr.size();
+
+    int sorted = 0;
+    while (!sorted){
+        sorted = 1;
+        for(int i = 0; i<n-1; i++){
+            if(arr[i]>arr[i+1]){
+                sorted = 0;
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+    }
+
+    for(int num: arr){
+        cout<<num<<" ";
+    }    
     
     return 0;
 }
