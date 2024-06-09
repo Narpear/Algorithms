6002a473a8bb22faa4346a188b9a011b90e94b14
Narpear <prer.kulk@gmail.com>
2024-06-09 16:19:24 +0530

    merge sort cpp

--- Sorting Algorithms/merge_sort.cpp
+++ Sorting Algorithms/merge_sort.cpp
@@ -3,10 +3,36 @@
 using namespace std;
 
 void merge(vector<int> &arr, int left, int right, int mid){
-    
+    vector<int> left_copy(arr.begin()+left, arr.begin()+mid+1);
+    vector<int> right_copy(arr.begin()+mid+1, arr.begin()+right+1);
+
+    int left_counter = 0, right_counter = 0;
+    int sorted_counter = left;
+
+    while(left_counter<left_copy.size() && right_counter<right_copy.size()){
+        if (left_copy[left_counter]<=right_copy[right_counter]){
+            arr[sorted_counter] = left_copy[left_counter];
+            left_counter++;
+        }
+        else{
+            arr[sorted_counter] = right_copy[right_counter];
+            right_counter++;
+        }
+        sorted_counter++;
+    }
+    while (left_counter<left_copy.size()){
+        arr[sorted_counter] = left_copy[left_counter];
+        left_counter++;
+        sorted_counter++;
+    }
+    while (right_counter<right_copy.size()){
+        arr[sorted_counter] = right_copy[right_counter];
+        right_counter++;
+        sorted_counter++;
+    }
 }
 
-void merge_sort(vector<int> &arr; int left; int right){
+void merge_sort(vector<int> &arr, int left, int right){
     if (left>=right)
         return;
     
