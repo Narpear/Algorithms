9c488d02c1aa4f2148d2948665d8378688ab9ea3
Narpear <prer.kulk@gmail.com>
2024-07-01 21:51:13 +0530

    kadanes algorithm

---
+++ Kadane's Algorithm/kadane_algorithm.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        # sliding window solution
+        # left pointer is incremented if the prefix sum becomes negative
+        # right pointer is always incremented
+
+        maxSub = nums[0]
+        current_sum = 0
+
+        for number in nums:
+            if current_sum<0:
+                current_sum = 0
+            current_sum += number
+            maxSub = max(maxSub, current_sum)
